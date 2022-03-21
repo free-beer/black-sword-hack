54245eddb2d8de58ae27d50169330c72697e792c
Peter Wood <pwood@blacknorth.com>
2022-03-21 09:12:54 +0000

    Reworked the demon sheet.

--- modules/sheets/demon-sheet.js
+++ modules/sheets/demon-sheet.js
@@ -1,4 +1,12 @@
 export default class DemonSheet  extends ItemSheet {
+	static get defaultOptions() {
+	    return(mergeObject(super.defaultOptions,
+	    	               {classes: ["bsh", "bsh-sheet", "bsh-demon"],
+	    	                height: 350,
+	    	               	template: "systems/black-sword-hack/templates/sheets/demon-sheet.html",
+	    	                width: 700}));
+	}
+
 	get template() {
 		return("systems/black-sword-hack/templates/sheets/demon-sheet.html");
 	}
