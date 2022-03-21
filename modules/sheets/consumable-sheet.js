3cfd9478557b6a8e874935ff889a71b024c70a43
Peter Wood <pwood@blacknorth.com>
2022-03-21 08:53:09 +0000

    Reworked the consumable sheet.

--- modules/sheets/consumable-sheet.js
+++ modules/sheets/consumable-sheet.js
@@ -1,4 +1,12 @@
 export default class ConsumableSheet  extends ItemSheet {
+	static get defaultOptions() {
+	    return(mergeObject(super.defaultOptions,
+	    	               {classes: ["bsh", "bsh-sheet", "bsh-consumable"],
+	    	                height: 450,
+	    	               	template: "systems/black-sword-hack/templates/sheets/consumable-sheet.html",
+	    	                width: 600}));
+	}
+
 	get template() {
 		return("systems/black-sword-hack/templates/sheets/consumable-sheet.html");
 	}
