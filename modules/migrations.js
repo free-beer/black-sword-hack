2fbdd6b866909e2cfd80d55fd6a147ff02a0e839
Peter Wood <pwood@blacknorth.com>
2023-03-04 21:13:57 +0000

    Small tweak to the migration.

--- modules/migrations.js
+++ modules/migrations.js
@@ -13,35 +13,38 @@ async function updateCharacterBackgrounds() {
 			if(typeof actor.system.backgrounds.first === "string" ||
 			   typeof actor.system.backgrounds.second === "string" ||
 			   typeof actor.system.backgrounds.third === "string") {
-				let updates = {system: {
-					                  backgrounds: {first: first, second: second, third:  third}
-					              }
-					          };
+                let updatable = true;
+				let updates   = {system: {
+					                    backgrounds: {first: first, second: second, third:  third}
+					                }
+					            };
 
                 console.log(`Attempting background migration for '${actor.name}' (id: ${actor.id}).`);
                 Object.values(actor.system.backgrounds).forEach((name) => {
                 	if(`${name}`.trim() !== "" && !CLASSIC_ORIGIN_MAP[name]) {
                 		console.error(`Unable to migrate the '${name}' background for character id '${actor.id}' (${actor.name}).`);
-                		throw(`Unable to migrate unrecognised character background '${name}' for ${actor.name}.`);
+                        updatable = false;
                 	}
                 });
 
-                if(first !== "" && typeof actor.system.backgrounds.first === "string") {
-                	console.log(`Migrating '${actor.system.backgrounds.first}' to`, CLASSIC_ORIGIN_MAP[first]);
-                	updates.system.backgrounds.first = CLASSIC_ORIGIN_MAP[first];
-                }
+                if(updatable) {
+                    if(first !== "" && typeof actor.system.backgrounds.first === "string") {
+                    	console.log(`Migrating '${actor.system.backgrounds.first}' to`, CLASSIC_ORIGIN_MAP[first]);
+                    	updates.system.backgrounds.first = CLASSIC_ORIGIN_MAP[first];
+                    }
 
-                if(second !== "" && typeof actor.system.backgrounds.second === "string") {
-                	console.log(`Migrating '${actor.system.backgrounds.second}' to`, CLASSIC_ORIGIN_MAP[second]);
-                	updates.system.backgrounds.second = CLASSIC_ORIGIN_MAP[second];
-                }
+                    if(second !== "" && typeof actor.system.backgrounds.second === "string") {
+                    	console.log(`Migrating '${actor.system.backgrounds.second}' to`, CLASSIC_ORIGIN_MAP[second]);
+                    	updates.system.backgrounds.second = CLASSIC_ORIGIN_MAP[second];
+                    }
 
-                if(third !== "" && typeof actor.system.backgrounds.third === "string") {
-                	console.log(`Migrating '${actor.system.backgrounds.third}' to`, CLASSIC_ORIGIN_MAP[third]);
-                	updates.system.backgrounds.third = CLASSIC_ORIGIN_MAP[third];
-                }
+                    if(third !== "" && typeof actor.system.backgrounds.third === "string") {
+                    	console.log(`Migrating '${actor.system.backgrounds.third}' to`, CLASSIC_ORIGIN_MAP[third]);
+                    	updates.system.backgrounds.third = CLASSIC_ORIGIN_MAP[third];
+                    }
 
-                actor.update(updates, {diff: true});
+                    actor.update(updates, {diff: true});
+                }
 			}
 		}
 	});
